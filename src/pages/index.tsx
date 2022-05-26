0fd0df5ba57aed527ed2187bcb50f1fa5cb99af5
rafAmorim8 <rafael_amorim30@hotmail.com>
2022-05-25 22:02:05 -0700

    Adds custom fonts and header info

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
+import Head from 'next/head';
 import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Hello World</h1>
-  )
+    <>
+      <Head>
+        <title>Start | ig.news</title>
+      </Head>  
+      <h1 className={styles.title}>Hello <span>World</span></h1>
+    </>
+  );
 }
