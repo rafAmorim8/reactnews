4af28bef5e2c74f6f3d22ba5b6178e98009b3bae
rafAmorim8 <rafael_amorim30@hotmail.com>
2022-05-30 22:39:19 -0700

    Adds session provider

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,30 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
+import { signIn, useSession, signOut } from 'next-auth/react';
 
 export function SignInButton(){
-  const isUserLoggedIn = true;
+  const session = useSession();
 
-  return isUserLoggedIn ? (
-    <button className={styles.signInButton}>
+  console.log(session.data);
+
+  return session.status === 'authenticated' ? (
+    <button 
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+      >
       <FaGithub color="#04D361"/>
-      rafAmorim8
+      {session.data.user.name}
       <FiX color="#737380" className={styles.closeIcon}/>
     </button>
   ) : (
-    <button className={styles.signInButton}>
-      <FaGithub color="#"/>
+    <button 
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn('github')}
+    >
+      <FaGithub color="#EBA417"/>
       Sign in with GitHub
     </button>
 
